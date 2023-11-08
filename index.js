b35cec79b88cddee9d62136ea684286b3a5833db
leen-gh <leen.ghaleb.d@gmail.com>
2023-11-08 05:19:14 +0300

    assigment is done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(name, event){
+    const arry =[]
+    for (let i = 0; i < name.length; i++) {
+        arry[i]= `Thank you, ${name[i]}, for the wonderful surprise gift!`
+        //debugger;
+    }
+    return arry
+}
+
+function countDown(num){
+    while (num >= 0) {
+        console.log(num--);
+    }
+}
